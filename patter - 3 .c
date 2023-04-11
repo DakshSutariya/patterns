704d50d2fcc25444d281ac58af9384a05a9d32dc
DakshSutariya <130463070+DakshSutariya@users.noreply.github.com>
2023-04-11 17:39:41 +0530

    Add files via upload

---
+++ patter - 3 .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ main()
+ 
+  
+{
+	printf("--------\n");
+	printf("|\t|\n");
+	printf("D\t|\n");
+	printf("A\t|\n");
+	printf("K\t|\n");
+	printf("S\t|\n");
+	printf("H\t|\n");
+	printf("|\t|\n");
+	printf(" -------");
+}
